05b0bb9e1a7bf3298e7780501dcf510b1d99e13b
banteg <banteeg@gmail.com>
2015-09-21 07:43:18 +0600

    add future options

--- itf.py
+++ itf.py
@@ -2,8 +2,6 @@ from __future__ import unicode_literals, print_function
 import requests
 import click
 
-print('Apple Music Festival 2015 Downloader\n')
-
 QUALITY = {
     '1080p': ('8500_256', '.ts'),
     '720p': ('3500_256', '.ts'),
@@ -20,10 +18,16 @@ def get_artist_id(artist):
 
 
 @click.command()
-@click.argument('tag')
+@click.argument('day', type=click.IntRange(19, 28))
 @click.argument('artist')
 @click.argument('quality', default='1080p', type=click.Choice(['1080p', '720p', 'ac3']))
-def main(tag, artist, quality):
+@click.option('--dump', '-d', is_flag=True, help='dump token and urls instead of downloading')
+@click.option('--proxy', '-p', help='specify proxy')
+def main(day, artist, quality, dump, proxy):
+    '''
+    Apple Music Festival 2015 Downloader
+    '''
+
     stream, ext = QUALITY[quality]
 
     if '_' not in artist:
@@ -32,10 +36,10 @@ def main(tag, artist, quality):
     token = requests.get('https://itunes.apple.com/apps1b/authtoken/token.txt').text
     cookies = {'token': token}
 
-    output = '{}_{}_{}{}'.format(tag, artist, quality, ext)
+    output = '{}_{}_{}{}'.format(day, artist, quality, ext)
 
     files_url = 'http://streaming.itunesfestival.com/auth/eu1/vod/201509{}/v1/{}/{}_vod.m3u8'
-    files = requests.get(files_url.format(tag, stream, artist), cookies=cookies, allow_redirects=False)
+    files = requests.get(files_url.format(day, stream, artist), cookies=cookies, allow_redirects=False)
 
     if 'performance_not_available' in files.headers.get('location', ''):
         print('Not available in your country, use proxy.')
@@ -51,7 +55,7 @@ def main(tag, artist, quality):
     for c, part in enumerate(files, 1):
         print('Downlading part {}/{} {}'.format(c, total, part))
         part_url = 'http://streaming.itunesfestival.com/auth/eu1/vod/201509{}/v1/{}/{}'
-        data = requests.get(part_url.format(tag, stream, part), cookies=cookies)
+        data = requests.get(part_url.format(day, stream, part), cookies=cookies)
         with open(output, 'ab') as f:
             f.write(data.content)
 
--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='itf',
-    version='2.0',
+    version='2.1',
     description='Download Apple Music Festival 2015 Streams',
     url='https://github.com/banteg/itf',
     py_modules=['itf'],
-    install_requires=['requests'],
+    install_requires=['requests', 'click'],
     entry_points={
         'console_scripts': ['itf = itf:main'],
     }
